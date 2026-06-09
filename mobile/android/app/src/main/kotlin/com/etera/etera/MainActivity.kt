869e82808c60e24278cddd7b7a7042eb0d75e555
Abel Ashenafi <ashenafiabel66@gmail.com>
2026-06-09 20:09:08 +0300

    Refactor the app icon and name

---
+++ mobile/android/app/src/main/kotlin/com/etera/etera/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.etera.etera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- mobile/android/app/src/main/kotlin/com/etera/etera_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.etera.etera_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
